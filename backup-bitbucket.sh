bdb8852173ea8535eb4991d70d14172a80906fad
Carles San Agustin <info@carlessanagustin.com>
2016-02-25 10:38:30 +0100

    backup to remote rsync command

--- backup-bitbucket.sh
+++ backup-bitbucket.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# backup to local
 java \
 -Dbitbucket.password="bitbucket_password" \
 -Dbitbucket.user="bitbucket_user" \
@@ -7,3 +8,10 @@ java \
 -Dbitbucket.home=/var/atlassian/application-data/bitbucket \
 -Dbackup.home=/bitbucket-backups \
 -jar /opt/atlassian/bitbucket-backup-client-2.0.2/bitbucket-backup-client.jar
+
+
+# backup to remote
+rsync -av\
+ --log-file="/bitbucket-backups/log/rsync.log.$(date +%Y%m%d%H%M)"\
+ /bitbucket-backups/backups/*\
+ rsync@192.168.1.1::project/bitbucket-backups/
